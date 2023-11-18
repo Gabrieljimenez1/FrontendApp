c5bedf5c8e2868794dbda2639a33cc34d196358b
Gabriel David Jimenez Mosquera <gabrieljimo@unisabana.edu.co>
2023-11-18 12:46:16 -0500

    Corrige metodo ServicioEmpleados

--- src/servicios/ServicioEmpleados.js
+++ src/servicios/ServicioEmpleados.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-const BASE_URL_API_EMPLEADO_TODOS =
-  "http://localhost:8080/perfil/buscar/todos";
+const BASE_URL_API_EMPLEADO_TODOS = "http://localhost:8080/perfil/buscar/todos";
 
 class ServicioEmpleados {
   getEmpleados() {
     return axios.get(BASE_URL_API_EMPLEADO_TODOS, {
       headers: {
         "Content-Type": "application/json",
-      }
+      },
     });
   }
 }
 
-export default new ServicioEmpleados();
+const servicioEmpleados = new ServicioEmpleados();
+export default servicioEmpleados;
