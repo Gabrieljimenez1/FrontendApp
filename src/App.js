120e14433877b0ff339e9836e76ad989f9eb9053
JuanJosÃeCifuentes <jcifuentesc03@gmail.com>
2023-11-20 17:10:47 -0500

    Agrega botones de ruteo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FooterComponent from "./componentes/FooterComponent";
 import HeaderComponent from "./componentes/HeaderComponent";
 import ListaEmpleados from "./componentes/ListaEmpleados";
+import ListaRoles from "./componentes/ListaRoles";
+import ListaCertificados from "./componentes/ListaCertificados";
+import ListaPerfiles from "./componentes/ListaPerfiles";
 
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-        <ListaEmpleados />
-      </div>
-      <FooterComponent />
+      <Router>
+          
+            <HeaderComponent />
+              <div className="container">
+                <Routes>
+                  <Route path="/" exact  element={<ListaEmpleados />}></Route>
+                  <Route path="/empleados" element={<ListaEmpleados />}></Route>
+                  <Route path="/roles" element={<ListaRoles /> }></Route>
+                  <Route path="/certificados" element={<ListaCertificados /> }></Route>
+                  <Route path="/perfiles" element={<ListaPerfiles /> }></Route>
+                </Routes>
+              </div>
+            <FooterComponent />
+          
+      </Router>
     </div>
+    
   );
 }
 
-export default App;
+export default App;
